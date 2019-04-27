97e969ceb9a331200db84601c6b15dfe3dce8717
Mahadesh2016 <mahadesh2016@gmail.com>
2019-04-27 16:43:00 +0530

    Updates

---
+++ precache-manifest.a1f9f5e85c23becfc74916708224f573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6204e9bc8061c6f5f26ee7320e2b1f",
+    "url": "/react-book-app-master/static/media/back1.9c6204e9.svg"
+  },
+  {
+    "revision": "b9691e95b2852bf135be",
+    "url": "/react-book-app-master/static/js/runtime~main.b9691e95.js"
+  },
+  {
+    "revision": "26b23d785ccceb8bfbf0",
+    "url": "/react-book-app-master/static/js/main.26b23d78.chunk.js"
+  },
+  {
+    "revision": "8a99abf8c95ad1de65be",
+    "url": "/react-book-app-master/static/js/1.8a99abf8.chunk.js"
+  },
+  {
+    "revision": "26b23d785ccceb8bfbf0",
+    "url": "/react-book-app-master/static/css/main.c2eea916.chunk.css"
+  },
+  {
+    "revision": "5339f9c89200a9466ddd7e9da9207bce",
+    "url": "/react-book-app-master/index.html"
+  }
+];
